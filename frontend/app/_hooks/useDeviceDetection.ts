47215645d896bb4fdfcae8d383d2d6147ae9d5fc
Olaoluwa Thompson <137652527+codakyello@users.noreply.github.com>
2025-05-10 11:32:09 +0100

    updates

--- frontend/app/_hooks/useDeviceDetection.ts
+++ frontend/app/_hooks/useDeviceDetection.ts
@@ -3,6 +3,10 @@ import { useEffect, useState } from "react";
 
 export default function useDeviceDetection() {
   const [{ isMobile, isTablet, isDesktop }, setDevice] = useState(() => {
+    if (typeof window === "undefined") {
+      return { isMobile: false, isTablet: false, isDesktop: false };
+    }
+
     const isMobile = window.innerWidth < 768;
     const isTablet = window.innerWidth >= 768 && window.innerWidth <= 1024;
     const isDesktop = window.innerWidth > 1024;
